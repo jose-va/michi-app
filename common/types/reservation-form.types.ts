d6dfbe0792221a458ec039bf60803eb672f7362f
jose-va <josemanuelvilchezarenas@gmail.com>
2026-05-21 08:54:38 +0200

    refactor: [RESERVATION] Modificar el apartado de reservas

---
+++ common/types/reservation-form.types.ts
@@ -0,0 +1,23 @@
+import { UseFormReturn } from "react-hook-form";
+import { z } from "zod";
+
+export const formSchema = z.object({
+  date: z.date(),
+  guests: z.string().min(1, "Seleccione el número de comensales."),
+  location: z.enum(["INSIDE", "OUTSIDE"]),
+  startTime: z.string(),
+  name: z.string().min(2, "Por favor, introduzca un nombre"),
+  phone: z.string().min(9, "El número de teléfono introducido no es correcto"),
+  email: z
+    .email("El correo electrónico introducido no es correcto")
+    .nullable()
+    .or(z.literal("")),
+  observations: z.string().nullable().or(z.literal("")),
+});
+
+export type ReservationFormValues = z.infer<typeof formSchema>;
+
+export interface ReservationFormProps {
+  form: UseFormReturn<ReservationFormValues>;
+}
+
