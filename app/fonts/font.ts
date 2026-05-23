0ecfa9fd8f808ff996b77611748875d7e739fae9
jose-va <josemanuelvilchezarenas@gmail.com>
2026-05-23 10:14:51 +0200

    feat: [FONTS]: Crear archivo con las fuentes

---
+++ app/fonts/font.ts
@@ -0,0 +1,13 @@
+import { Bodoni_Moda, Montserrat } from "next/font/google";
+import { Playfair_Display } from "next/font/google";
+
+export const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const bodoni = Bodoni_Moda({
+    subsets: ["latin"],
+});
+
+
+export const playfair = Playfair_Display({
+  subsets: ["latin"],
+});
