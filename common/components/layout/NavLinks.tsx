83316aaf98241c9f1e0ce1637bf5497e32cb02d7
jose-va <josemanuelvilchezarenas@gmail.com>
2026-05-26 17:40:06 +0200

    fix: [LINK]: Solucionado bug en el Link

--- common/components/layout/NavLinks.tsx
+++ common/components/layout/NavLinks.tsx
@@ -1,28 +1,28 @@
 "use client";
-import Link from "next/link";
 import {
   NavigationMenuItem,
+  NavigationMenuLink,
   NavigationMenuList,
+  navigationMenuTriggerStyle,
 } from "@/shadcn/components/navigation-menu";
 
 const navItems = [
   { label: "Carta", href: "/product" },
-  { label: "Ubicacion", href: "/location" },
   { label: "Reservar", href: "/reservation" },
+  { label: "Ubicacion", href: "/location" },
 ];
 
 export default function NavLinks() {
-
   return (
     <NavigationMenuList className="ml-1 flex justify-center md:gap-4">
       {navItems.map((item) => (
         <NavigationMenuItem key={item.href}>
-          <Link
+          <NavigationMenuLink
             href={item.href}
-            className="text-[14.5px] font-bold text-white/90 hover:text-white md:text-[15px]"
+            className={`${navigationMenuTriggerStyle()} text-[14.5px] font-bold text-white/90 hover:text-white md:text-[15px]`}
           >
             {item.label}
-          </Link>
+          </NavigationMenuLink>
         </NavigationMenuItem>
       ))}
     </NavigationMenuList>
